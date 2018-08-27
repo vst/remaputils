1c371846cc34d5352fdd819b15ddbb0c38247925
Tolga Sezer <tolgasbox@gmail.com>
2018-08-27 12:45:10 +0200

    Finalised first version of sync.R with documentation.

--- R/sync.R
+++ R/sync.R
@@ -1,3 +1,15 @@
+##' A function to sync portfolios between 2 DECAF instances.
+##'
+##' This is the description
+##'
+##' @param sourceSession The session info for the DECAF instance to sync from.
+##' @param targetSession The session info for the DECAF instance to sync to.
+##' @param accounts The account/portfolio mapper between the 2 instances.
+##' @param teamGUID The team GUID of the target instance.
+##' @param exclude A vector with key words to exclude unwanted columns in source portfolios.
+##' @return The function creates the portfolios in the target instance and return the source portfolio data-frame.
+##' @import rdecaf
+##' @export
 syncPortfolios <- function(sourceSession, targetSession, accounts, teamGUID, exclude=NULL) {
 
     ## Construct the vision account params:
@@ -14,7 +26,7 @@ syncPortfolios <- function(sourceSession, targetSession, accounts, teamGUID, exc
     sourcePortfoliosOriginal <- sourcePortfolios
 
     ## Overwrite the team field:
-    sourcePortfolios[, "team"] <- "dcf:team?guid=f401ee72-60a3-496a-9504-da8dd1d67b3e"
+    sourcePortfolios[, "team"] <- paste0("dcf:team?guid=", teamGUID)
 
     ## Exclude columns from source portfolio frame:
     if (!is.null(exclude)) {
@@ -37,12 +49,21 @@ syncPortfolios <- function(sourceSession, targetSession, accounts, teamGUID, exc
     response <- pushPayload(payload=payload, endpoint=NULL, session=targetSession, import=FALSE, inbulk=TRUE, params=list(sync="True"))
 
     ## Done, return:
-    list("response"=response,
-         "portfolios"=sourcePortfoliosOriginal)
+    sourcePortfoliosOriginal
 
 }
 
 
+##' A function to sync share classes between 2 DECAF instances.
+##'
+##' This is the description
+##'
+##' @param sourceSession The session info for the DECAF instance to sync from.
+##' @param targetSession The session info for the DECAF instance to sync to.
+##' @param sourcePortfolios The data-frame with the source portfolios.
+##' @return The function creates the share classes in the target instance and returns a share class data-frame.
+##' @import rdecaf
+##' @export
 syncShareclass <- function(sourceSession, targetSession, sourcePortfolios){
 
     ## Get the params:
@@ -65,16 +86,22 @@ syncShareclass <- function(sourceSession, targetSession, sourcePortfolios){
         sourceShreclasses <- sourceShreclasses[, -grep(fld, colnames(sourceShreclasses))]
     }
 
+    ## Match the portfolio:
     sourcePortfolioID <- sourcePortfolios[match(sourceShreclasses[, "portfolio"], sourcePortfolios[, "id"]), "id"]
+
+    ## Get the share-class information for portfolios in question:
     sourceShreclasses <- sourceShreclasses[!is.na(sourcePortfolioID), ]
 
+    ## Iterate over source share classes and create where necessary:
     for (row in 1:NROW(sourceShreclasses)) {
 
+        ## Get the nth share-class:
         shrcls <- sourceShreclasses[row,]
 
+        ## Tyr to match the individual source share-class and the target share-classes:
         matchShrcls <- match(trimConcatenate(shrcls[, "name"]), trimConcatenate(safeTry(try(safeColumn(targetShreclasses, "name"), silent=TRUE))))
 
-        ## Hebele:
+        ## If such share-class exists in target, overwrite fields and return:
         if (any(!is.na(matchShrcls))) {
             sourceShreclasses[row, "portfolio"] <- targetShreclasses[matchShrcls, "portfolio"]
             sourceShreclasses[row, "id"] <- targetShreclasses[matchShrcls, "id"]
@@ -82,15 +109,18 @@ syncShareclass <- function(sourceSession, targetSession, sourcePortfolios){
             next
         }
 
+        ## If share-class in target doesn't exist, overwrite the necessary fields to create such share-class:
         sourcePortfolioGUID   <-   sourcePortfolios[match(shrcls[, "portfolio"], sourcePortfolios[, "id"]), "guid"]
         shrcls[, "portfolio"] <- targetPortfolios[match(sourcePortfolioGUID, targetPortfolios[, "guid"]), "id"]
         shrcls[, "id"] <- NULL
-        ## shrcls[, "guid"] <- NULL
 
+        ## Create the payload:
         payload <- toJSON(as.list(shrcls),  auto_unbox=TRUE)
 
+        ## Post the payload:
         response <- postResource("shareclasses", payload=payload, session=targetSession)
 
+        ## Overwrite the fields:
         sourceShreclasses[row, "portfolio"] <- shrcls[, "portfolio"]
         sourceShreclasses[row, "id"] <- response[["id"]]
         sourceShreclasses[row, "guid"] <- response[["guid"]]
@@ -102,6 +132,81 @@ syncShareclass <- function(sourceSession, targetSession, sourcePortfolios){
 }
 
 
+##' A function to sync agents between 2 DECAF instances:
+##'
+##' This is the description
+##'
+##' @param sourceSession The session info for the DECAF instance to sync from.
+##' @param targetSession The session info for the DECAF instance to sync to.
+##' @param tradeAgents The agent id's coming from the source trades.
+##' @return The function creates the agents in the target instance and returns a agent class data-frame.
+##' @import rdecaf
+##' @export
+syncAgent <- function(sourceSession, targetSession, tradeAgents){
+
+    ## Get the params:
+    params <- list("format"="csv", "page_size"=-1)
+
+    ## Get the system portfolios:
+    sourceAgents <- as.data.frame(getResource("agents", params=params, session=sourceSession))
+
+    ## Filter the agents by the trade's agents:
+    sourceAgents <- sourceAgents[match(unique(na.omit(tradeAgents)), sourceAgents[, "id"]), ]
+
+    ## Append the source id's:
+    sourceAgents[, "source_id"] <- sourceAgents[, "id"]
+
+    ## The return value:
+    retval <- sourceAgents
+
+    ## Get the system portfolios:
+    targetAgents <- as.data.frame(getResource("agents", params=params, session=targetSession))
+
+    ##  Get rid of unnecessary fields:
+    for (fld in c("created", "creator", "updated", "updater")) {
+        sourceAgents <- sourceAgents[, -grep(fld, colnames(sourceAgents))]
+    }
+
+    ## If we have any agents in target, filter source agents which do not exist in target:
+    if (!NROW(targetAgents) == 0) {
+        sourceAgents <- sourceAgents[is.na(match(targetAgents[, "guid"], sourceAgents[, "guid"])), ]
+    }
+
+    ## If all source agents exist in target, exit and return retval:
+    if (NROW(sourceAgents) == 0) {
+        return(retval)
+    }
+
+    ## Iterate over agents and create:
+    for (row in 1:NROW(sourceAgents)) {
+
+        ## Set id to NULL:
+        sourceAgents[, "id"] <- NULL
+
+        ## Create payload:
+        payload <- toJSON(as.list(sourceAgents), auto_unbox=TRUE)
+
+        ## Push payload:
+        response <- pushPayload(payload, "agents", session=targetSession, import=FALSE, inbulk=FALSE, params=NULL)
+
+    }
+
+    ## Done, return:
+    retval
+
+}
+
+
+##' A function to sync resources between 2 DECAF instances:
+##'
+##' This is the description
+##'
+##' @param sourceSession The session info for the DECAF instance to sync from.
+##' @param targetSession The session info for the DECAF instance to sync to.
+##' @param sourceAccounts The accounts data-frame coming from the source instance.
+##' @return The function creates the resources in the target instance and returns a resource data-frame.
+##' @import rdecaf
+##' @export
 syncResources <- function(targetSession, sourceSession, sourceAccounts) {
 
     ## Get the vision stocks:
@@ -118,6 +223,7 @@ syncResources <- function(targetSession, sourceSession, sourceAccounts) {
     ## Import the underlyings:
     if (any(resources[, "is_underlying"])) {
 
+        ## Get the resources which are underlyings:
         underlyings <- list("artifacts"=resources[resources[, "is_underlying"], ])
 
         ## Create the payload:
@@ -131,7 +237,7 @@ syncResources <- function(targetSession, sourceSession, sourceAccounts) {
     payload <- toJSON(list("artifacts"=resources), auto_unbox=TRUE, na="null", digits=10)
 
     ## Push the payload:
-    response <- pushPayload(payload=payload, endpoint=NULL, session=session, import=FALSE, inbulk=TRUE, params=list(sync="True"))
+    response <- pushPayload(payload=payload, endpoint=NULL, session=targetSession, import=FALSE, inbulk=TRUE, params=list(sync="True"))
 
     ## Done, return:
     safeRbind(lapply(getResource("resources", params=list("page_size"=-1), session=targetSession), function(res) do.call(cbind, res[!names(res) == "tags"])))
@@ -139,6 +245,16 @@ syncResources <- function(targetSession, sourceSession, sourceAccounts) {
 }
 
 
+##' A function to sync analytical types between 2 DECAF instances:
+##'
+##' This is the description
+##'
+##' @param sourceSession The session info for the DECAF instance to sync from.
+##' @param targetSession The session info for the DECAF instance to sync to.
+##' @param sourceAccounts The accounts data-frame coming from the source instance.
+##' @return The function creates the analytical types in the target instance and returns a analytical types data-frame.
+##' @import rdecaf
+##' @export
 syncAtypes <- function(sourceSession, targetSession, sourceAccounts) {
 
     ## Get the source atypes:
@@ -166,6 +282,17 @@ syncAtypes <- function(sourceSession, targetSession, sourceAccounts) {
 }
 
 
+##' A function to sync institutions between 2 DECAF instances:
+##'
+##' This is the description
+##'
+##' @param sourceSession The session info for the DECAF instance to sync from.
+##' @param targetSession The session info for the DECAF instance to sync to.
+##' @param sourceAccounts The accounts data-frame coming from the source instance.
+##' @param trades If trades is given, additional institutions based on feeagt will be created. Default is NULL.
+##' @return The function creates the institutions in the target instance and returns a institutions data-frame.
+##' @import rdecaf
+##' @export
 syncInstitutions <- function(sourceSession, targetSession, sourceAccounts, trades=NULL) {
 
     ## Get the source atypes:
@@ -185,6 +312,9 @@ syncInstitutions <- function(sourceSession, targetSession, sourceAccounts, trade
     ## Remove duplicate institutions and NA's:
     sourceInstitutions <- cleanNARowsCols(sourceInstitutions[!duplicated(sourceInstitutions[, "id"]), ])
 
+    ## Retain the source id:
+    sourceInstitutions[, "source_id"] <- sourceInstitutions[, "id"]
+
     ## Overwrite resource fields:
     for (fld in c("id", "created", "creator", "updated", "updater")) {
         sourceInstitutions[, fld] <- NULL
@@ -202,6 +332,19 @@ syncInstitutions <- function(sourceSession, targetSession, sourceAccounts, trade
 }
 
 
+##' A function to sync accounts between 2 DECAF instances:
+##'
+##' This is the description
+##'
+##' @param sourceSession The session info for the DECAF instance to sync from.
+##' @param targetSession The session info for the DECAF instance to sync to.
+##' @param atypes The data-frame with analtyical types.
+##' @param institutions The data-frame with institutions.
+##' @param portfolios The data-frame with portfolios.
+##' @param accounts The data-frame with the source accounts.
+##' @return The function creates the accounts in the target instance and returns a accounts data-frame.
+##' @import rdecaf
+##' @export
 syncAccounts <- function(sourceSession, targetSession, atypes, institutions, portfolios, accounts) {
 
     ## Overwrite resource fields:
@@ -222,7 +365,6 @@ syncAccounts <- function(sourceSession, targetSession, atypes, institutions, por
     accounts[, "portfolio"] <- paste0("dcf:portfolio?guid=",
                                       portfolios[match(accounts[, "portfolio_name"], portfolios[, "name"]), "guid"])
 
-
     ## Create the payload:
     payload <- toJSON(list("accounts"=accounts), auto_unbox=TRUE, na="null", digits=10)
 
@@ -231,5 +373,91 @@ syncAccounts <- function(sourceSession, targetSession, atypes, institutions, por
 
     ## Return:
     accounts
+
+}
+
+
+##' A function to sync trades between 2 DECAF instances:
+##'
+##' This is the description
+##'
+##' @param targetSession The session info for the DECAF instance to sync to.
+##' @param trades The trades from the target instance.
+##' @param atypes The data-frame with analtyical types.
+##' @param shrclss The data-frame with the share classes.
+##' @param institutions The data-frame with institutions.
+##' @param agents The data-frame with agents.
+##' @return The function creates the trades in the target instance and returns a trades data-frame.
+##' @import rdecaf
+##' @export
+syncTrades <- function(targetSession, trades, atypes, shrclss, institutions, agents) {
+
+    ## Overwrite accmain with guid:
+    trades[, "accmain"] <- ifelse(is.na(trades[, "accmain"]), NA, paste0("dcf:account?guid=", trades[, "accmain_guid"]))
+
+    ## Overwrite accaltn with guid:
+    trades[, "accaltn"] <- ifelse(is.na(trades[, "accaltn"]), NA, paste0("dcf:account?guid=", trades[, "accaltn_guid"]))
+
+    ## Overwrite acccomp with guid:
+    trades[, "acccomp"] <- ifelse(is.na(trades[, "acccomp"]), NA, paste0("dcf:account?guid=", trades[, "acccomp_guid"]))
+
+    ## Overwrite accundr with guid:
+    trades[, "accundr"] <- ifelse(is.na(trades[, "accundr"]), NA, paste0("dcf:account?guid=", trades[, "accundr_guid"]))
+
+    ## Overwrite acccntr with guid:
+    trades[, "acccntr"] <- ifelse(is.na(trades[, "acccntr"]), NA, paste0("dcf:account?guid=", trades[, "acccntr_guid"]))
+
+    ## Overwrite accprin with guid:
+    trades[, "accprin"] <- ifelse(is.na(trades[, "accprin"]), NA, paste0("dcf:account?guid=", trades[, "accprin_guid"]))
+
+    ## Overwrite accintr with guid:
+    trades[, "accintr"] <- ifelse(is.na(trades[, "accintr"]), NA, paste0("dcf:account?guid=", trades[, "accintr_guid"]))
+
+    ## Overwrite resmain with guid:
+    trades[, "resmain"] <- ifelse(is.na(trades[, "resmain"]), NA, paste0("dcf:artifact?guid=", trades[, "resmain_guid"]))
+
+    ## Overwrite resaltn with guid:
+    trades[, "resaltn"] <- ifelse(is.na(trades[, "resaltn"]), NA, paste0("dcf:artifact?guid=", trades[, "resaltn_guid"]))
+
+    ## Overwrite rescomp with guid:
+    trades[, "rescomp"] <- ifelse(is.na(trades[, "rescomp"]), NA, paste0("dcf:artifact?guid=", trades[, "rescomp_guid"]))
+
+    ## Overwrite resundr with guid:
+    trades[, "resundr"] <- ifelse(is.na(trades[, "resundr"]), NA, paste0("dcf:artifact?guid=", trades[, "resundr_guid"]))
+
+    ## Overwrite rescntr with guid:
+    trades[, "rescntr"] <- ifelse(is.na(trades[, "rescntr"]), NA, paste0("dcf:artifact?guid=", trades[, "rescntr_guid"]))
+
+    ## Overwrite resprin with guid:
+    trades[, "resprin"] <- ifelse(is.na(trades[, "resprin"]), NA, paste0("dcf:artifact?guid=", trades[, "resprin_guid"]))
+
+    ## Overwrite resintr with guid:
+    trades[, "resintr"] <- ifelse(is.na(trades[, "resintr"]), NA, paste0("dcf:artifact?guid=", trades[, "resintr_guid"]))
+
+    ## Overwrite atype with guid:
+    trades[, "atype"]   <- ifelse(is.na(trades[, "atype"]), NA, paste0("dcf:analyticaltype?guid=", atypes[match(trades[, "atype"], atypes[, "source_id"]), "guid"]))
+
+    ## Overwrite agent with guid:
+    trades[, "agent"]   <- ifelse(is.na(trades[, "agent"]), NA, paste0("dcf:agent?guid=", agents[match(trades[, "agent"], agents[, "source_id"]), "guid"]))
+
+    ## Overwrite shrcls" with guid:
+    trades[, "shrcls"]  <- ifelse(is.na(trades[, "shrcls"]), NA, paste0("dcf:shareclass?guid=", shrclss[match(trades[, "shrcls"], shrclss[, "source_id"]), "guid"]))
+
+    ## Overwrite feeagt" with guid:
+    trades[, "feeagt"]  <- ifelse(is.na(trades[, "feeagt"]), NA, paste0("dcf:institution?guid=", institutions[match(trades[, "feeagt"], institutions[, "source_id"]), "guid"]))
+
+    ## Get rid of columns:
+    for (fld in c("id")) {
+        trades[, fld] <- NULL
+    }
+
+    ## Create the payload:
+    payload <- toJSON(list("actions"=trades), auto_unbox=TRUE, na="null", digits=10)
+
+    ## Push the payload:
+    response <- pushPayload(payload=payload, endpoint=NULL, session=session, import=FALSE, inbulk=TRUE, params=list(sync="True"))
+
+    ## Done, return:
+    trades
 
 }

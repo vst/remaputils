522ff9034fc91ae7c86395b7855ad3cc961d9df1
Tolga Sezer <tolgasbox@gmail.com>
2018-09-04 11:54:13 +0700

    Added syncUpdateEmail function

--- R/email.R
+++ R/email.R
@@ -49,3 +49,47 @@ emailReport <- function(from, emailList, subject, body, isLocal=FALSE, html=TRUE
               html=html,
               authenticate=TRUE)
 }
+
+
+##' A function to send system emails using the an HTML email template.
+##'
+##' The function searces for keywords in an HTML template and replaces
+##' the keywords with some other HTML string.
+##'
+##' @param template The HTML template.
+##' @param updateText A list with the keyword and the corresponding replacement string.
+##' @param emailParams The parameter list for the email report function.
+##' @param timezone The timezone to be used. Default is CET.
+##' @return A java-object of class org.apache.commons.mail.SimpleEmail
+##' @export
+syncUpdateEmail <- function(template, updateText, emailParams, timezone="CET") {
+
+    ## Get the system time:
+    sysTime <- format(Sys.time(), "tz"=timezone)
+
+    ## Iterate over updateText and replace placeholders:
+    for (i in 1:length(updateText)) {
+
+        ## Get the name of placeholder:
+        plcHolder <- names(updateText)[[i]]
+
+        ## Get the replacement text:
+        rplcText <- updateText[[i]]
+
+        ## Get the line which has placeholder:
+        isPlcHolder <- do.call(c, lapply(template, function(x) length(grep(plcHolder, x)) > 0))
+
+        ## Replace the line with the placeholder:
+        template[isPlcHolder] <- gsub(plcHolder, rplcText, template[isPlcHolder])
+    }
+
+    ## Construct the subject:
+    subject <- paste0(updateText[["DEPLOYMENT"]], " DECAF Data Update: ", sysTime)
+
+    ## Push the email:
+    emailReport(emailParams[["from"]],
+                emailParams[["emailList"]],
+                subject=subject,
+                body=paste(template, collapse=""),
+                emailParams[["isLocal"]])
+}

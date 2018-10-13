5c98a670aef2508ef331e39d8aeb2ace23195016
Tolga Sezer <tolgasbox@gmail.com>
2018-10-13 12:19:18 +0700

    Added the dubiousOHLCcodes function to health.R

--- R/health.R
+++ R/health.R
@@ -1,3 +1,41 @@
+##' This function checks whether ohlccodes are dubious.
+##'
+##' This is a description
+##'
+##' @param resources A data-frame with the resources from rdecaf.
+##' @param jaccardCoeff The threshold for the jaccard string distance coefficient.
+##' @return The resource data-frame with dubious ohlccodes.
+##' @export
+dubiousOHLCcodes <- function(resources, jaccardCoeff) {
+
+    ## Get the resources which have OHLC codes:
+    resourceWithOHLCcode <- resources[!isNAorEmpty(resources[, "ohlccode"]), ]
+
+    ## If none has ohlccodes, return NULL:
+    if (NROW(resourceWithOHLCcode) == 0) {
+        return(NULL)
+    }
+
+    ## Which ones fail to grep the ohlccode in symbol:
+    grepMethod <- apply(resourceWithOHLCcode, MARGIN=1, function(row) safeGrep(row["symbol"], row["ohlccode"]) == "0")
+
+    ## Which ones have a very dissimilar ohlccode compared to symbol:
+    jaccardMethod <- as.numeric(apply(resourceWithOHLCcode, MARGIN=1, function(x) stringdist(as.character(x["symbol"]), as.character(x["ohlccode"]), method="jaccard"))) > jaccardCoeff
+
+    ## Get the dubious ohlc codes:
+    dubiousOHLCcodes <- resourceWithOHLCcode[grepMethod | jaccardMethod,]
+
+    ## If none dubious, return NULL:
+    if (NROW(dubiousOHLCcodes) == 0) {
+        return(NULL)
+    }
+
+    ## Done, return:
+    dubiousOHLCcodes
+
+}
+
+
 ##' This function checks whether trade dates are dubious.
 ##'
 ##' This is a description

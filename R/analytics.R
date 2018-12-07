c26aee0753da450b6ba5fb5c75ff56e8803c23fb
Tolga Sezer <tolgasbox@gmail.com>
2018-12-07 16:59:42 +0700

    Initiated the optionsDelta function in analytics.R

--- R/analytics.R
+++ R/analytics.R
@@ -1,3 +1,18 @@
+##' A function to produce the option deltas.
+##'
+##' This is the description
+##'
+##' @param holdings The holdings.
+##' @param resources The resources.
+##' @return The option deltas.
+##' @export
+optionDelta <- function(holdings, resources) {
+
+    bscallimpvol(s=2649, k=2800, r=0.08, tt=as.numeric(as.Date("2018-12-21") - Sys.Date()) / 365, d=0, price=5.5)
+
+
+}
+
 ##' A function to detect outliers in xts return series:
 ##'
 ##' This is the description

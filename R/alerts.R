ec12bb8a77279d3b9671568321f272903230bacc
Tolga Sezer <tolgasbox@gmail.com>
2019-07-23 16:57:34 +0700

    Added the alertLatestTrades

--- R/alerts.R
+++ R/alerts.R
@@ -136,3 +136,103 @@ performanceOutliers <- function(portfolio, start=NULL, factor=8, session) {
     ## Return:
     NULL
 }
+
+
+##' A function to send latest trades alert.
+##'
+##' This is the description
+##'
+##' @param session The rdecaf session.
+##' @param accounts The data-frame with the rdecaf accounts.
+##' @param resources The data-frame of the resources in the decaf instance.
+##' @param emailParams The parameters for the email dispatch.
+##' @param hours The number of hours to look back.
+##' @param greeting The greetings string.
+##' @param deployment The name of the deployment / client.
+##' @param url The url of the deployment.
+##' @param gte Greater than or equal to this time (HH:MM:SS) to run this alert.
+##' @param lte Less than or equal to this time (HH:MM:SS) to run this alert.
+##' @param tz The time-zone for gte and lte.
+##' @return NULL. Email with the alert will be sent.
+##' @export
+alertLatestTrades <- function(session,
+                              accounts,
+                              resources,
+                              emailParams,
+                              hours=24,
+                              greeting="",
+                              deployment="",
+                              url="",
+                              gte="10:01:00",
+                              lte="10:19:00",
+                              tz="UTC") {
+
+    ## Is it alert time?
+    itsAlertTime <- itsTime(tz=tz, gte=gte, lte=lte)
+
+    ## If it is not alert time, return NULL:
+    if (!itsAlertTime) {
+        return(NULL)
+    }
+
+    ## Prepare the params for the trades:
+    params <- list("page_size"=-1, "format"="csv", "commitment__gte"=Sys.Date() - 90)
+
+    ## Get the trades:
+    trades <- as.data.frame(getResource("trades", params=params, session=session))
+
+    ## Filter out the cash trades:
+    trades <- trades[trades[, "resmain_ctype"] != "CCY", ]
+
+    ## Filter the latest trades:
+    trades <- trades[difftime(as.POSIXct(format(Sys.time(), tz="UTC")), as.POSIXct(trades[, "created"]), units="hours") < hours, ]
+
+    ## If no trades, mask trade data frame:
+    if (NROW(trades) == 0) {
+        trades <- initDF(colnames(trades), 1)
+    }
+
+    ## Construct the trade links:
+    tradeLink <- paste0(gsub("api", "", session[["location"]]), "trade/details/", trades[, "id"])
+
+    ## Construct the report email data-frame:
+    result <- data.frame("Trade Link"=tradeLink,
+                         "Account"=trades[, "accmain_name"],
+                         "Type"=resources[match(trades[, "resmain"], resources[, "id"]), "ctype"],
+                         "Symbol"=ellipsify(trades[, "resmain_symbol"], 16),
+                         "Name"=ellipsify(resources[match(trades[, "resmain"], resources[, "id"]), "name"], 25),
+                         "Trade Date"=trades[, "commitment"],
+                         "QTY"=beautify(as.character(round(as.numeric(trades[, "qtymain"]), 2)), nsmall=2),
+                         "PX"=beautify(as.character(round(as.numeric(trades[, "pxmain"]), 2)), nsmall=2),
+                         check.names=FALSE,
+                         stringsAsFactors=FALSE)
+
+    ## HTMLize the links:
+    result[, "Trade Link"] <- paste0("<a href='", result[, "Trade Link"], "'>LINK</a>")
+
+    ## Generate the outlier table as HTML and convert to string:
+    result <- as.character(emailHTMLTable(result,
+                                          provider="DECAF",
+                                          caption=paste0("Trades Created In The Last ", hours, " Hours"),
+                                          sourceType="API"))
+
+    ## Clean the HTML text:
+    result <- gsub("&#62;LINK&#60;/a&#62;", ">LINK<aya/a>", result)
+    result <- gsub("&#60;a href", "<a href", result)
+
+    .UPDATETEXT <- list("GREETINGPLACEHOLDER"=greeting,
+                        "EMAILBODYPLACEHOLDER"="Your DECAF system has been updated with the latest data snapshots available ",
+                        "CALLTOACTIONPLACEHOLDER"="Go to System",
+                        "DEPLOYMENT"=deployment,
+                        "URLPLACEHOLDER"=url,
+                        "FINALPARAGRAPHPLACEHOLDER"="Please contact us if you experience any issues or have questions/feedback.",
+                        "ADDRESSPLACEHOLDER"="",
+                        "GOODBYEPLACEHOLDER"="Best Regards,<br>DECAF TEAM",
+                        "ADDENDUMPLACEHOLDER"=result)
+
+    syncUpdateEmail(template=readLines("../assets/update_email.html"),
+                     updateText=.UPDATETEXT,
+                     emailParams=emailParams,
+                     subject=" DECAF Data Update: ")
+
+}

a908d2a8d53b5fc62f16bd6ce1fd8455e7f04006
Tolga Sezer <tolgasbox@gmail.com>
2020-01-05 20:10:05 +0700

    Extension to ansbacherCommons

--- R/ansbacherCommons.R
+++ R/ansbacherCommons.R
@@ -43,7 +43,7 @@ ansbacherSecTypeMapper <- function() {
 ##'
 ##' @return Returns a list with the column mappers.
 ##' @export
-ansbacherColumnMappers <- function() {
+ansbacherCLIV001Columns <- function() {
     ## Columns to be selected:
     list("ACCDATE"="POSDATE",
          "REFCCY"="REFCCY",
@@ -72,7 +72,13 @@ ansbacherColumnMappers <- function() {
          "PVAL_SECURITY_LIBLG1S"="SECDESC2",
          "PVAL_SECURITY_FRQREVS"="FREQ",
          "PVAL_SECURITY_TXINTD"="INTEREST",
-         "PVAL_SHORTTERM_TXINTD"="INTEREST_SHORTTERM",
+         "PVAL_SHORTTERM_TXINTD"="SHRTRM_INTEREST",
+         "PVAL_SHORTTERM_TERMEFIXEB"="SHRTRM_FIXEDTERM",
+         "PVAL_SHORTTERM_DTCREAL"="SHRTRM_ISSUEDT",
+         "PVAL_SHORTTERM_VALACHDEVTITD"="SHRTRM_QTY",
+         "PVAL_SHORTTERM_LIBEXTS"="SHRTRM_ISSUER_DESC",
+         "PVAL_SHORTTERM_EMETTEURLBLGS"="SHRTRM_LONG_DESC",
+         "PVAL_SHORTTERM_REVANESTIMEVALD"="SHRTRM_ESTINCOME",
          "PVAL_SECURITY_DTEMISSL"="ISSUEDATE",
          "PVAL_SECURITY_TYPQTS"="QUOTETYPE",
          "PVAL_SECURITY_CRSD"="PXLAST",
@@ -87,4 +93,114 @@ ansbacherColumnMappers <- function() {
          "PVAL_FOREX_XRATEFRXD"="FWDRATE",
          "PVAL_FOREX_XRATEFRXTERMED"="CURRENTFWDRATE",
          "PVAL_FOREX_MTPNLD"="FXFWDPNL")
+}
+
+
+##' A function to return the column mappers
+##'
+##' This is the description
+##'
+##' @return Returns a list with the column mappers.
+##' @export
+ansbacherITGI002Columns <- function() {
+    ## Columns to be selected:
+    list("H__DTSYSL"="SYSDATE",
+         "H__DTCPTL"="ACCDATE",
+         "P__IDCLL"="ACCOUNT",
+         "P__IDPERSL"="CLIENT",
+         "O__CDAPPS"="APPCODE",
+         "O__DTCPTL"="BOOKDATE",
+         "O__FICHES"="OPRCODE",
+         "O__DTEXEL"="EXEDATE",
+         "O__DTVALL"="VALDATE",
+         "O__LIBOPS"="TXNTYPE",
+         "O__MVTCTA_LIBCPS"="TXNDESC",
+         "O__TYPINS"="SECTYPE",
+         "D__IOI"="DEBCRED",
+         "O__CDOPS"="TXNCODE",
+         "D__DEVCPTEXTS"="ACCFXCODE",
+         ##"D__DEVCPTS"="ACCTYPE",
+         ##"D__DEVOPS"="TXNCCY",
+         "D__DEVCPTS"="TXNCCY",
+         "D__DEVOPS"="ACCTYPE",
+         "D__DEVOPEXTS"="TXNCCYCODE",
+         "D__XRATETRND"="SECACCFX",
+         "D__MTFRSD"="FEEAMOUNT",
+         ##"D__MTNETCPTD"="NETAMOUNTREF",
+         ##"D__MTNETDEVOPD"="NETAMOUNTSEC",
+         "D__MTNETCPTD"="NETAMOUNTSEC",
+         "D__MTNETDEVOPD"="NETAMOUNTREF",
+         "D__MTD"="QTY",
+         "D__MTCOMD"="COMAMOUNT",
+         "D__MTCOURTD"="BRKAMOUNT",
+         "D__IDTIS"="SECID",
+         "D__GRES"="SECTYPECODE",
+         "D__IDPRIVS"="SECIDENT",
+         "D__LIBTIT1S"="SECDESC1",
+         "D__LIBTIT2S"="SECDESC2",
+         "D__LIBTIT3S"="SECDESC3",
+         "D__QTD"="SECQTY",
+         "D__CRSTID"="PXCOST",
+         "D__CDPYEXTS"="SECCOUNTRY",
+         "D__CDBSEXTS"="SECMARKET",
+         "D__MTCOMBQED"="BANKCOMM",
+         "D__MTCOUD"="COUPON")
+}
+
+
+ansbacherParser <- function(data, fileType) {
+
+    ## Get the column mappers:
+    columns <-  do.call(paste0("ansbacher", fileType, "Columns"), list())
+
+    ## Get the columns:
+    for (col in names(columns)) {
+        data[, col] <- safeColumn(data, col)
+    }
+
+    ##
+    data <- data[, names(columns)]
+
+    ## Assign new column names:
+    colnames(data) <- as.character(sapply(columns, identity))
+
+    data[, "FREQ"] <- safeColumn(data, "FREQ")
+
+    ## Parse the FREQ column:
+    data[isNAorEmpty(as.character(data[, "FREQ"])), "FREQ"] <- NA
+
+    ## Swtich the FREQ column:
+    data[, "FREQ"] <- sapply(data[, "FREQ"], function(f) switch(as.character(f), "yearly"=1, "half-yearly"=2, "quarterly"=4, "NA"=NA))
+
+    ## Done, return:
+    data
+
+}
+
+
+ansbacherFilePreemble <- function(data) {
+
+    if (any(safeGrep(colnames(data), "REPNAME") == "1")) {
+        colName <- "REPNAME"
+    }
+
+    if (any(safeGrep(colnames(data), "H__REPORTNAME") == "1")) {
+        colName <- "H__REPORTNAME"
+    }
+
+    ## ## Get the file type (position or transaction?)
+    fileType <- gsub("_", "", as.character(data[, colName])[1])
+
+    if (colName == "REPNAME") {
+        ## Exclude 'group' client types:
+        data <- data[data[, "CLIENT_TYPE"] == "portfolio", ]
+    }
+
+    ## Parse the ansbacher data:
+    data <- ansbacherParser(data, fileType)
+
+    ## Done, return:
+    return(list("data"=data,
+                "type"=fileType))
+
 }
